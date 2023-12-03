9d93c3298ea772c69dfbff9b44b9e1f3183ed92a
Tomislav Radanovic <8278883+TommyRadan@users.noreply.github.com>
2023-12-03 03:13:20 +0100

    cmake: make doxygen and clang-format optional
    This commit makes doxygen and clang-format dependencies optional.
    If the dependencies are missing, the targets related to them are
    not generated.

--- cmake/generate-docs.cmake
+++ cmake/generate-docs.cmake
@@ -1,12 +1,12 @@
 # Look for Doxygen package
 find_package(Doxygen)
-if(DOXYGEN_FOUND)
-    add_custom_target(docs
-        COMMAND ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/docs/config.doxy
-        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/docs
-        COMMENT "Generating API documentation with Doxygen"
-        VERBATIM
-    )
-else(DOXYGEN_FOUND)
-    message(STATUS "Doxygen needs to be installed to generate the doxygen documentation")
-endif(DOXYGEN_FOUND)
+if(NOT DOXYGEN_FOUND)
+    return()
+endif(NOT DOXYGEN_FOUND)
+
+add_custom_target(docs
+    COMMAND ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/docs/config.doxy
+    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/docs
+    COMMENT "Generating API documentation with Doxygen"
+    VERBATIM
+)
